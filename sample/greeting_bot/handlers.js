36f43d64d78b5d44458dc41ef5c5ae08cd06667e
masayuki-ioki <masayuki-ioki@cookpad.com>
2018-05-29 18:32:17 +0900

    Add test

---
+++ sample/greeting_bot/handlers.js
@@ -0,0 +1,18 @@
+const HelloIntent = function() {
+  this.response.speak('Hello! Good day.');
+  this.response.listen('Hello! Good day.');
+  this.emit(":responseReady");
+};
+
+const ByeIntent = function() {
+  this.response.speak('See you!');
+  this.emit(":responseReady");
+};
+
+const LaunchRequest = function() {
+  this.response.speak('Welocome to GreetingBot');
+  this.response.listen('Welocome to GreetingBot');
+  this.emit(":responseReady");
+};
+
+export { LaunchRequest, HelloIntent, ByeIntent };
